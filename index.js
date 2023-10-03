0a47b7a747a6d302a14e5ed4c7d4463acc9f52a2
root <+>
2023-10-04 00:47:15 +0300

    Assignment complete

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function (a, b) {
+    return 2000 / 100;
+  }
+  const square = () => {
+    let x = 2;
+    return x * x;
+  }
+
+  const add = () => {
+    let a = 3;
+    let b = 4;
+    return a + b;
+  }
